2163732a4d5240b1b6da03ef275f744b3cba0817
Luca Andaloro <luca.andaloro4@gmail.com>
2022-02-24 15:25:33 +0100

    mail migliorato

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,20 +1,35 @@
 let inputMail = document.querySelector("form").email;
 let logForm = document.querySelector("form");
 
+let check = document.createElement("h1");
+
 const users= ['asohaoh@gmail.com', 'asjfhaihiu@gmail.com', 'ciao@live.it', 'ciaociao@hotmail.it', 'hello@libero.it']
 
 logForm.addEventListener("submit", log);
 
 function log(e){
     let registered = false;
-    
+
     for(i=0; i < users.length; i++){
         if(users[i]==inputMail.value){
             registered = true;
         }
     }
 
-    registered ? console.log('ci sei fra!') : console.log('non sei registrato');
+    registered ? userConfirmed() : userNotConfirmed();
 
     e.preventDefault();
+}
+
+
+function userConfirmed(){
+    check.innerText = ('ci sei fra!');
+
+    logForm.append(check);
+}
+
+function userNotConfirmed(){
+    check.innerText = ('non sei registrato');
+
+    logForm.appendChild(check);
 }
